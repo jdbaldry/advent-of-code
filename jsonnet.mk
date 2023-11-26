5c64a841c64e74d6ebd54c331217b116057e38ce
Jack Baldry <jack.baldry@grafana.com>
2023-11-26 17:38:36 +0000

    Simplify Jsonnet Makefile
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- jsonnet.mk
+++ jsonnet.mk
@@ -1,7 +1,12 @@
 ALL += main.json
 
-JSONNET_ARGS += -J $(GIT_ROOT)/lib
+JSONNET_ARGS  := -J ../../lib
+JSONNET_FILES := $(shell find ./ -name '*.jsonnet')
 
-main.json: ## Evaluate the Jsonnet solution
-main.json: main.jsonnet input.txt
-	jsonnet $(JSONNET_ARGS) $< > $@
+main.json: ## Evaluate the Jsonnet solution.
+main.json: main.jsonnet $(JSONNET_FILES) input.txt
+	jsonnet $(JSONNET_ARGS) $< | tee $@
+
+%.json: ## Evaluate any other Jsonnet.
+%.json: %.jsonnet $(JSONNET_FILES) input.txt
+	jsonnet $(JSONNET_ARGS) $< | tee $@
