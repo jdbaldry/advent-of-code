70ff52e30708cd130c9019df351fad806a8bcb9c
Jack Baldry <jack.baldry@grafana.com>
2023-11-26 10:57:05 +0000

    Add common target for fetching input
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- common.mk
+++ common.mk
@@ -11,7 +11,17 @@ help: ## Display this help.
 help:
 	@awk 'BEGIN {FS = ": ##"; printf "Usage:\n  make <target>\n\nTargets:\n"} /^[a-zA-Z0-9_\.\-\/%]+: ##/ { printf "  %-45s %s\n", $$1, $$2 }' $(MAKEFILE_LIST)
 
-GIT_ROOT = $(shell git rev-parse --show-toplevel)
+GIT_ROOT := $(shell git rev-parse --show-toplevel)
+
+input.txt: ## Fetch the input for the puzzle.
+ifndef AOC_SESSION_COOKIE
+	$(error the AOC_SESSION_COOKIE environment variable is required to fetch the input file)
+endif
+	$(eval DAY := $(notdir $(CURDIR)))
+	$(eval YEAR := $(notdir $(realpath ..)))
+	curl -sLo $(@) \
+		-H 'Cookie: session=$(AOC_SESSION_COOKIE)' \
+		https://adventofcode.com/$(YEAR)/day/$(DAY:0%=%)/input
 
 .PHONY: all
 all: ## Run all solutions
